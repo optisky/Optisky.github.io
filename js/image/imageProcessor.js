ddb30eb5ef4ea48b57e27d8cf2138d4a3c92d958
optisky <hochung@yeah.net>
2020-05-20 15:01:27 +0800

    update

--- js/image/imageProcessor.js
+++ js/image/imageProcessor.js
@@ -26,26 +26,94 @@ function RGBAImage (width, height, data) {
 	this.type = 'RGBAImage';
 	this.width = width;
 	this.height = height;
-	this.data = new Uint8Array(weight*height*4);
+	this.data = new Uint8Array(width*height*4);
 	data && this.data.set(data);
+};
+
+RGBAImage.prototype.getPixel = function (row, column) {
+	var pos = (row*this.width + column)*4;
+	return new Color (
+		this.data[pos],
+		this.data[pos+1],
+		this.data[pos+2],
+		this.data[pos+3]
+	);
+};
+
+RGBAImage.prototype.setPixel = function (row, column, color) {
+	var pos = (row*this.width + column)*4;
+	this.data[pos]   = color.r;
+	this.data[pos+1] = color.g;
+	this.data[pos+2] = color.b;
+	this.data[pos+3] = color.a;
+	this.imread = function (img, cvs) {
+	var w = img.width;
+	var h = img.height;
 	
-	this.getPixel = function (row, column) {
-		var pos = (row*this.width + column)*4;
-		return new Color (
-			this.data[pos],
-			this.data[pos+1],
-			this.data[pos+2],
-			this.data[pos+3]
-		);
-	};
+	// resize the canvas for drawing the image
+	cvs.width = w;
+	cvs.height = h;
+	var ctx = cvs.getContext('2d');
 	
-	this.setPixel = function (row, column, color) {
-		var pos = (row*this.width + column)*4;
-		this.data[pos]   = color.r;
-		this.data[pos+1] = color.g;
-		this.data[pos+2] = color.b;
-		this.data[pos+3] = color.a;
-	};
+	ctx.drawImage(img, 0, 0);
+	var imgData = ctx.getImageData(0, 0, w, h);
+	var newImage = new RGBAImage(w, h, imgData);
+	
+	// clear up the old image and canvas
+	imgData = null;
+	ctx.clearRect(0, 0, w, h);
+	return newImage;
+};
+};
+
+RGBAImage.imread = function (img, cvs) {
+	var w = img.width;
+	var h = img.height;
+	
+	// resize the canvas for drawing the image
+	cvs.width = w;
+	cvs.height = h;
+	var ctx = cvs.getContext('2d');
+	
+	ctx.drawImage(img, 0, 0);
+	var imgData = ctx.getImageData(0, 0, w, h);
+	var newImage = new RGBAImage(w, h, imgData);
+	
+	// clear up the old image and canvas
+	imgData = null;
+	ctx.clearRect(0, 0, w, h);
+	return newImage;
+};
+
+RGBAImage.prototype.imread = function (img, cvs) {
+	var w = img.width;
+	var h = img.height;
+	
+	// resize the canvas for drawing the image
+	cvs.width = w;
+	cvs.height = h;
+	var ctx = cvs.getContext('2d');
+	
+	ctx.drawImage(img, 0, 0);
+	var imgData = ctx.getImageData(0, 0, w, h);
+	var newImage = new RGBAImage(w, h, imgData);
+	
+	// clear up the old image and canvas
+	imgData = null;
+	ctx.clearRect(0, 0, w, h);
+	return newImage;
+};
+	
+RGBAImage.prototype.toImageData = function (ctx) {
+	var imgData = ctx.createImageData(this.width, this.height);
+	imgData.data.set(this.data);
+	return imgData;
+};
+
+RGBAImage.prototype.render = function (cvs) {
+	canvas.width = this.width;
+	canvas.height = this.height;
+	context.putImageData(this.toImageData(context, 0, 0));
 };
 
 /*************** Color Object ***************/
@@ -58,16 +126,29 @@ function Color (r, g, b, a) {
 		this.b = b;
 		this.a = a;
 	};
-	
-	this.setColor = function (that) {
-		if (that != null && that.constructor === Color) {
-			this.r = that.r;
-			this.g = that.g;
-			this.b = that.b;
-			this.a = that.a;
-		} else {
-			return null;
-		}
-	};
+};
+
+Color.prototype.setColor = function (that) {
+	if (that != null && that.constructor === Color) {
+		this.r = that.r;
+		this.g = that.g;
+		this.b = that.b;
+		this.a = that.a;
+	} else {
+		return null;
+	}
 };
 	
+/*************** Image Loader ****************/
+function ImageLoader () {
+	this.result = undefined;
+	this.loadImage = function (imgsrc, cvs) {
+		var that = this;
+		var img = new Image();
+		img.onload = function () {
+			that.result = RGBAImage.imread(img, cvs);
+			that.result.render(cvs);
+		};
+		img.src = imgsrc;
+	};
+};
